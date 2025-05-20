9e21332e01401e6b046345fbd6fe6d2c997bf042
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-05-21 04:11:56 +0500

    Initial

---
+++ CS01-Lesson-04-01-ConsApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS01_Lesson_04_01_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
