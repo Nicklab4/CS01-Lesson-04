e9209e1e378132380b6e718a53cf0cf9a0198041
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-05-23 01:34:28 +0500

    CS01-Lesson-04-01-ConsApp

--- CS01-Lesson-04-01-ConsApp/Program.cs
+++ CS01-Lesson-04-01-ConsApp/Program.cs
@@ -10,6 +10,42 @@ namespace CS01_Lesson_04_01_ConsApp
     {
         static void Main(string[] args)
         {
+            //
+            // Объявление массивов
+            //
+
+            int[] array0;
+            array0 = new int[5];
+
+            int[] array1 = new int[5];
+
+            int[] array2 = new int[] { 1, 3, 5, 7, 9 };
+
+            int[] array3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //
+            // Вывод массива
+            //
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", array3[i]);
+            }
+            Console.WriteLine();
+
+            //
+            // Возведение каждого элемента в квадрат
+            //
+
+            for (int i = 0; i < 10; i++)
+            {
+                array3[i] = array3[i] * array3[i];
+                Console.Write(array3[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+
         }
     }
 }
