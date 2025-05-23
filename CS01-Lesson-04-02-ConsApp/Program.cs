58f32af2559d0566e46a3e443982ce6352413c26
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-05-23 05:58:13 +0500

    CS01-Lesson-04-02-ConsApp

---
+++ CS01-Lesson-04-02-ConsApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS01_Lesson_04_02_ConsApp
+{
+    class Program
+    {
+        //
+        // Массив как параметр
+        //
+
+        static void Print(int n , int[] a)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write("{0} ", a[i]);
+        }
+
+        static void Change(int n, int[] a)
+        {
+            for (int i = 0; i < n; i++)
+                if (a[i] < 0) a[i] = -a[i];
+        }
+
+        static void Main(string[] args)
+        {
+            int[] myArray = { 0, -1, -2, 3, 4, -6, -7, 5, 8, -9 };
+            Print(10, myArray);
+            Console.WriteLine();
+            Change(10, myArray);
+            Print(10, myArray);
+
+            Console.ReadLine();
+        }
+    }
+}
