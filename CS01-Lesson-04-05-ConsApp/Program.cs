248c3ce25a809a3bb639b053d142f0ca6c6b73fc
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-05-28 02:30:02 +0500

    CS01-Lesson-04-05-ConsApp

---
+++ CS01-Lesson-04-05-ConsApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Исключения
+//Функции обработки исключений на языке C# помогают обрабатывать любые непредвиденные или
+//исключительные ситуации, происходящие при выполнении программы.При обработке исключений
+//используются ключевые слова try, catch и finally.
+
+
+namespace CS01_Lesson_04_05_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag;
+
+            do
+            {
+                Console.WriteLine("Введите число:");
+                try
+                {
+                    flag = false;
+                    int a = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Неверный формат данных");
+                    Console.WriteLine(ex.Message);
+                    flag = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Неправильно ввели данные");
+                    Console.WriteLine(ex.Message);
+                    flag = true;
+                }
+                finally
+                {
+                    Console.WriteLine("finally");
+                }
+
+
+            } while (flag);
+
+            Console.ReadLine();
+        }
+    }
+}
