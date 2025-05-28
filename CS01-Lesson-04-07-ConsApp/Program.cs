742cfada25ab986b52b50b8184e5a9ff4ddbb407
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-05-29 03:30:44 +0500

    CS01-Lesson-04-07-ConsApp

---
+++ CS01-Lesson-04-07-ConsApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CS01_Lesson_04_07_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("..\\..\\data.txt");
+
+            int sum = 0, count = 0;
+
+            while (!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+                Console.WriteLine("Считали строку: " + s);
+                try
+                {
+                    int a = int.Parse(s);
+                    sum = sum + a;
+                    count++;
+                }
+                catch (Exception ex )
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            sr.Close();
+
+            Console.WriteLine("Среднее арифметическое:{0:f2}", (double)sum/count);
+
+            Console.ReadLine();
+        }
+    }
+}
