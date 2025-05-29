97785395d71928520d240bbf0538669ced1a48c4
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2025-05-30 00:01:00 +0500

    CS01-Lesson-04-08-ConsApp

---
+++ CS01-Lesson-04-08-ConsApp/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Задача 1. Класс «Мой одномерный массив»
+
+//Разработать класс для работы с одномерным массивом.Предусмотреть конструктор, заполняющий
+//массив конкретными значениями, и конструктор, заполняющий массив случайными числами.Сделать
+//методы поиска среднего значения, максимального элемента массива, минимального элемента
+//массива, подсчета количества положительных чисел и метод, возвращающий массив в виде строки.
+
+
+
+namespace CS01_Lesson_04_08_ConsApp
+{
+    class MyArray
+    {
+        int[] a;
+
+        public MyArray(int n , int el)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = el;
+        }
+
+        public MyArray(int n, int min, int max)
+        {
+            a = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next(min, max);
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] > max) max = a[i];
+                return max;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                int min = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] < min) min = a[i];
+                return min;
+            }
+        }
+
+        public int CountPositiv
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] > 0) count++;
+                return count;
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int v in a)
+                s = s + v + " ";
+            return s;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray a = new MyArray(10, 0, 100);
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(a.Max);
+            Console.WriteLine(a.Min);
+            Console.WriteLine(a.CountPositiv);
+
+            Console.ReadLine();
+        }
+    }
+}
